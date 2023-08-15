586655a26cf14fa73ab9f724f9c27813bb21a47e
adarsh singh <94890074+adarsh1704@users.noreply.github.com>
2023-08-15 16:11:07 +0530

    created the menu concept for the DSA concepts

--- DSAConcepts/Program.cs
+++ DSAConcepts/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, from DSAConcepts!");
+﻿
+string input= String.Empty;
+
+do
+{
+    DisplayMenu();
+    switch (input)
+    {
+        case "1":
+            {
+                Console.WriteLine("Going to perform the bitwise operations.");
+                break;
+            }
+        case "#":
+            {
+                Console.WriteLine("Exiting the application. Thankyou for using");
+                break;
+            }
+
+        default:
+            {
+                Console.WriteLine("****** Invalid Input ******\nPlease enter the valid input.");
+                break;
+            }
+    }
+} while (input != "#");
+
+
+void DisplayMenu()
+{
+
+    Console.WriteLine("******** Application Menu ********\n");
+    Console.WriteLine("1. Bitwise Operations\n");
+
+    Console.WriteLine("#. Exit\n");
+    Console.WriteLine("Please enter your input");
+    input = Convert.ToString(Console.ReadLine());
+
+}
